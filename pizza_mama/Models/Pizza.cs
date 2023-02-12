c1d6fa07900347a1e319fdf96e6b88c64386e3c0
joel <joeldowono1@gmail.com>
2023-02-12 23:52:34 +0100

    Modification de l'espace d'administration

--- pizza_mama/Models/Pizza.cs
+++ pizza_mama/Models/Pizza.cs
@@ -1,11 +1,17 @@
-﻿namespace pizza_mama.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pizza_mama.Models
 {
     public class Pizza
     {
         public int PizzaID { get; set; }
+        [Display(Name = "Nom")]
         public string nom { get;set; }
+        [Display(Name = "Prix (€)")]
         public float prix { get; set; }
-        public bool vegetarienne { get; set; }  
+        [Display(Name = "Végétarienne")]
+        public bool vegetarienne { get; set; }
+        [Display(Name = "Ingrédients")]
         public string ingredients { get; set; }
     }
 }
